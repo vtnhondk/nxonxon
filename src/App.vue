b52ab5e5f8770e9d8da5ea4afb1c856118bd8911
Nhơn Võ <nhon@horical.com>
2018-10-02 17:07:19 +0700

    ReInit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ComingSoon author="Nhơn Võ" year="2018"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComingSoon from "./components/ComingSoon.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    ComingSoon
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./styles/app.css"/>
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <h1>{{ author }} - {{ year }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComingSoon",
+  props: {
+    author: String,
+    year: String
+  }
+};
+</script>
+
+<style>
+.header {
+  margin: 0;
+  padding: 15px;
+}
+
+h1 {
+  font-size: 1em;
+}
+</style>
